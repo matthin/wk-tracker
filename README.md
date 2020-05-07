3be063ab988719a10650663d00aaa96f9a96c2e8
Justin Harrison <me@justinharrison.ca>
2020-05-07 15:00:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# WaniKani Tracker
+Tracks WaniKani review sessions.
+
+# Building
+    npm install
+    npm run build:firefox
+
+# License
+Licensed under GPL-3.0-only.
---
+++ src/index.ts
@@ -0,0 +1,53 @@
+interface Item {
+  readonly category: string;
+  readonly name: string;
+}
+
+function isItem(obj: any): obj is Item {
+  return (
+    obj !== "undefined" &&
+    typeof obj.category === "string" &&
+    typeof obj.name === "string"
+  );
+}
+
+function extractItems(elements: NodeListOf<Element>): Item[] {
+  return Array.from(elements).map(e => {
+    return {
+      category: e.getAttribute("class"),
+      name: e.textContent
+    };
+  }).filter(isItem);
+}
+
+async function saveSession(correct: Item[], incorrect: Item[], d: Date) {
+  const latest = await getStorage("latest") as number;
+  if (d <= new Date(latest)) {
+    return;
+  }
+
+  await browser.storage.local.set({
+    "latest": d.getTime()
+  });
+
+  await setStorage(d.getTime(), { correct: correct, incorrect: incorrect });
+}
+
+async function getStorage(key: string): Promise<any> {
+  const r = await browser.storage.local.get(key);
+  return r[key];
+}
+
+async function setStorage(k: number, v: any): Promise<void> {
+  await browser.storage.local.set({[k]: JSON.stringify(v)});
+}
+
+(async function() {
+  const correct = extractItems(document.querySelectorAll("#correct li"));
+  const incorrect = extractItems(document.querySelectorAll("#incorrect li"));
+
+  const sessionDate = document.querySelector("#last-session-date time")
+                              ?.getAttribute("datetime")!;
+  
+  await saveSession(correct, incorrect, new Date(sessionDate));
+})();
